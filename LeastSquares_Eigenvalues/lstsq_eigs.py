b255ca172ddba5e83dbf59fea6ee7a48e2be85bb
ElaineSwanson <swanscas@oregonstate.edu>
2021-03-12 05:09:20 -0800

    Lab 5

--- LeastSquares_Eigenvalues/lstsq_eigs.py
+++ LeastSquares_Eigenvalues/lstsq_eigs.py
@@ -1,17 +1,18 @@
 # lstsq_eigs.py
 """Volume 1: Least Squares and Computing Eigenvalues.
-<Name>
-<Class>
-<Date>
+<Name> Elaine Swanson
+<Class> MTH 420
+<Date> 2/10/2021
 """
+
+import numpy as np
+from scipy import linalg as la
+from matplotlib import pyplot as plt
 
 # (Optional) Import functions from your QR Decomposition lab.
 # import sys
 # sys.path.insert(1, "../QR_Decomposition")
 # from qr_decomposition import qr_gram_schmidt, qr_householder, hessenberg
-
-import numpy as np
-from matplotlib import pyplot as plt
 
 
 # Problem 1
@@ -24,77 +25,88 @@ def least_squares(A, b):
         b ((m, ) ndarray): A vector of length m.
 
     Returns:
-        x ((n, ) ndarray): The solution to the normal equations.
-    """
-    raise NotImplementedError("Problem 1 Incomplete")
+        x ((n, ) ndarray): The solution to the normal equations."""
+    Q,R = la.qr(A,mode="economic")
+    b2 = Q.T @ b #Or we can do... np.dot(Q.T, b)
+    x = la.solve_triangular(R, b2)
+    
+    return x
+
+A = np.random.random((5,2))
+b = np.random.random(5)
+print(least_squares(A, b))
 
 # Problem 2
 def line_fit():
     """Find the least squares line that relates the year to the housing price
     index for the data in housing.npy. Plot both the data points and the least
-    squares line.
-    """
-    raise NotImplementedError("Problem 2 Incomplete")
+    squares line."""
+    x,b = np.load('housing.npy').T
+    A = np.column_stack((x,np.ones((x.shape[0], 1)))) #stack col of 1s to matrix
+    k, m = least_squares(A, b)
+    plt.scatter(x, b)
+    plt.plot([0,16],[m,10*k+m])
+    return plt.show()
 
+print(line_fit())
 
 # Problem 3
 def polynomial_fit():
     """Find the least squares polynomials of degree 3, 6, 9, and 12 that relate
     the year to the housing price index for the data in housing.npy. Plot both
-    the data points and the least squares polynomials in individual subplots.
-    """
-    raise NotImplementedError("Problem 3 Incomplete")
+    the data points and the least squares polynomials in individual subplots."""
+    
+    x,b = np.load("housing.npy").T
+    """x_dup = np.column_stack([x]*4) #stack col four times vertically
+    A_m = np.column_stack((x_dup,np.ones((x_dup.shape[0], 1)))) 
+    A12 = np.vander(A_m[:,0], 13)
+    A9 = np.vander(A_m[:,1], 10)
+    A6 = np.vander(A_m[:,2], 7)
+    A3 = np.vander(A_m[:,3], 4)  
+    
+    x12, res12, rnk12, s12 = la.lstsq(A12,b) #Won't work with getting x alone, why?
+    x9, res9, rnk9, s9 = la.lstsq(A9,b)
+    x6, res6, rnk6, s6 = la.lstsq(A6,b)
+    x3, res3, rnk3, s3 = la.lstsq(A3,b)
+    
+    #x: least-squares solution. Return shape matches shape of b.
+    #residue: square of the 2-norm for each column in b - a x, if M > N and ndim(A) == n
+    #s: singular values of a."""
+    
+    p12 = np.poly1d(np.polyfit(x,b,12)) #Polynomial with coef. from np.polyfit
+    p9 = np.poly1d(np.polyfit(x,b,9))
+    p6 = np.poly1d(np.polyfit(x,b,6))
+    p3 = np.poly1d(np.polyfit(x,b,3))
 
+    x_fig = np.linspace(0,16)
+    y12_fig = p12(x_fig)
+    y9_fig = p9(x_fig)
+    y6_fig = p6(x_fig)
+    y3_fig = p3(x_fig)
+    
+    fig, axs = plt.subplots(2, 2)
+    fig.suptitle('Polynomial curve fitting')
+    fig.subplots_adjust(hspace=.5)
 
-def plot_ellipse(a, b, c, d, e):
-    """Plot an ellipse of the form ax^2 + bx + cxy + dy + ey^2 = 1."""
-    theta = np.linspace(0, 2*np.pi, 200)
-    cos_t, sin_t = np.cos(theta), np.sin(theta)
-    A = a*(cos_t**2) + c*cos_t*sin_t + e*(sin_t**2)
-    B = b*cos_t + d*sin_t
-    r = (-B + np.sqrt(B**2 + 4*A)) / (2*A)
+    axs[0, 0].scatter(x, b, marker = '.', color = 'steelblue', label = 'Housing data')
+    axs[0, 0].plot(x_fig, y12_fig, color = 'orangered', label = 'Fit line')
+    axs[0, 0].set_title('Polynomials of degree 12')
+    axs[0, 0].legend(loc = 'lower right')
 
-    plt.plot(r*cos_t, r*sin_t)
-    plt.gca().set_aspect("equal", "datalim")
+    axs[0, 1].scatter(x, b, marker = '.', color = 'steelblue', label = 'Housing data')
+    axs[0, 1].plot(x_fig, y9_fig, color = 'orangered', label = 'Fit line')
+    axs[0, 1].set_title('Polynomials of degree 9')
+    axs[0, 1].legend(loc = 'lower right')
 
-# Problem 4
-def ellipse_fit():
-    """Calculate the parameters for the ellipse that best fits the data in
-    ellipse.npy. Plot the original data points and the ellipse together, using
-    plot_ellipse() to plot the ellipse.
-    """
-    raise NotImplementedError("Problem 4 Incomplete")
-
-
-# Problem 5
-def power_method(A, N=20, tol=1e-12):
-    """Compute the dominant eigenvalue of A and a corresponding eigenvector
-    via the power method.
-
-    Parameters:
-        A ((n,n) ndarray): A square matrix.
-        N (int): The maximum number of iterations.
-        tol (float): The stopping tolerance.
-
-    Returns:
-        (float): The dominant eigenvalue of A.
-        ((n,) ndarray): An eigenvector corresponding to the dominant
-            eigenvalue of A.
-    """
-    raise NotImplementedError("Problem 5 Incomplete")
-
-
-# Problem 6
-def qr_algorithm(A, N=50, tol=1e-12):
-    """Compute the eigenvalues of A via the QR algorithm.
-
-    Parameters:
-        A ((n,n) ndarray): A square matrix.
-        N (int): The number of iterations to run the QR algorithm.
-        tol (float): The threshold value for determining if a diagonal S_i
-            block is 1x1 or 2x2.
-
-    Returns:
-        ((n,) ndarray): The eigenvalues of A.
-    """
-    raise NotImplementedError("Problem 6 Incomplete")
+    axs[1, 0].scatter(x, b, marker = '.', color = 'steelblue', label = 'Housing data')
+    axs[1, 0].plot(x_fig, y6_fig, color = 'orangered', label = 'Fit line')
+    axs[1, 0].set_title('Polynomials of degree 6')
+    axs[1, 0].legend(loc = 'lower right')
+    
+    axs[1, 1].scatter(x, b, marker = '.', color = 'steelblue', label = 'Housing data')
+    axs[1, 1].plot(x_fig, y3_fig, color = 'orangered', label = 'Fit line')
+    axs[1, 1].set_title('Polynomials of degree 3')
+    axs[1, 1].legend(loc = 'lower right')
+    
+    return plt.show()
+print(polynomial_fit())
