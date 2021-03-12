dfac9f48c34524303e1fd8b9e08a723d6e22a877
ElaineSwanson <swanscas@oregonstate.edu>
2021-03-12 05:03:08 -0800

    Lab 6

--- DrazinInverse/drazin.py
+++ DrazinInverse/drazin.py
@@ -1,8 +1,8 @@
 # drazin.py
 """Volume 1: The Drazin Inverse.
-<Name>
-<Class>
-<Date>
+<Name> Elaine Swanson
+<Class> MTH 420
+<Date> 3/4/2021
 """
 
 import numpy as np
@@ -17,8 +17,7 @@ def index(A, tol=1e-5):
         A ((n,n) ndarray): An nxn matrix.
 
     Returns:
-        k (int): The index of A.
-    """
+        k (int): The index of A."""
 
     # test for non-singularity
     if not np.isclose(la.det(A), 0):
@@ -39,21 +38,33 @@ def index(A, tol=1e-5):
 
 
 # Problem 1
+A = np.array([[1,3,0,0],[0,1,3,0],[0,0,1,3],[0,0,0,0]])
+Ad = np.array([[1,-3,9,81],[0,1,-3,-18],[0,0,1,3],[0,0,0,0]])
+ka=1
+B = np.array([[1,1,3],[5,2,6],[-2,-1,-3]])
+Bd = np.array([[0,0,0],[0,0,0],[0,0,0]])
+kb=3
 def is_drazin(A, Ad, k):
     """Verify that a matrix Ad is the Drazin inverse of A.
-
     Parameters:
         A ((n,n) ndarray): An nxn matrix.
         Ad ((n,n) ndarray): A candidate for the Drazin inverse of A.
         k (int): The index of A.
-
     Returns:
-        (bool) True of Ad is the Drazin inverse of A, False otherwise.
-    """
-    raise NotImplementedError("Problem 1 Incomplete")
-
-
+        (bool) True of Ad is the Drazin inverse of A, False otherwise."""
+    if (np.allclose(np.dot(A,Ad), np.dot(Ad,A)) 
+        and np.allclose(np.dot(np.linalg.matrix_power(A,k+1), Ad), np.linalg.matrix_power(A, k)) 
+        and np.allclose(np.dot(np.dot(Ad,A),Ad), Ad)):
+   #np.allclose(): checks if two arrays are element-wise equal within a tolerance.
+        return True
+    else:
+        return False
+print(is_drazin(A, Ad, ka))
+print(is_drazin(B, Bd, kb))
+                
 # Problem 2
+A = np.array([[0,0,2],[-3,2,6],[0,0,1]])
+
 def drazin_inverse(A, tol=1e-4):
     """Compute the Drazin inverse of A.
 
@@ -63,10 +74,25 @@ def drazin_inverse(A, tol=1e-4):
     Returns:
        ((n,n) ndarray) The Drazin inverse of A.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    n, n = np.shape(A)
+    T1, Q1, k1 = la.schur(A, sort = lambda x: abs(x) > tol) #Sort the Schur decomposition with 0 eigenvalues last.
+    T2, Q2, k2 = la.schur(A, sort = lambda x: abs(x) <= tol) #Sort the Schur decomposition with 0 eigenvalues first.
+    
+    U = np.column_stack((Q1[:, :k1], Q2[:, :n-k1])) 
+    U_inv = np.linalg.inv(U)
+    V = np.dot(np.dot(U_inv, A),U)
+    
+    Z = np.zeros((n, n))
+    V1 = V[:k1,:k1]
 
+    if k1 != 0:
+        M_inv = np.linalg.inv(V1)
+        Z[:k1,:k1] = M_inv
+    return np.dot(np.dot(U, Z), U_inv) #UZU^(-1)
+print(drazin_inverse(A, tol=1e-4))
 
 # Problem 3
+A = np.array([[0,1,0,0],[1,0,1,0],[0,1,0,1],[0,0,1,0]])
 def effective_resistance(A):
     """Compute the effective resistance for each node in a graph.
 
@@ -77,51 +103,18 @@ def effective_resistance(A):
         ((n,n) ndarray) The matrix where the ijth entry is the effective
         resistance from node i to node j.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
-
-
-# Problems 4 and 5
-class LinkPredictor:
-    """Predict links between nodes of a network."""
-
-    def __init__(self, filename='social_network.csv'):
-        """Create the effective resistance matrix by constructing
-        an adjacency matrix.
-
-        Parameters:
-            filename (str): The name of a file containing graph data.
-        """
-        raise NotImplementedError("Problem 4 Incomplete")
-
-
-    def predict_link(self, node=None):
-        """Predict the next link, either for the whole graph or for a
-        particular node.
-
-        Parameters:
-            node (str): The name of a node in the network.
-
-        Returns:
-            node1, node2 (str): The names of the next nodes to be linked.
-                Returned if node is None.
-            node1 (str): The name of the next node to be linked to 'node'.
-                Returned if node is not None.
-
-        Raises:
-            ValueError: If node is not in the graph.
-        """
-        raise NotImplementedError("Problem 5 Incomplete"
-
-
-    def add_link(self, node1, node2):
-        """Add a link to the graph between node 1 and node 2 by updating the
-        adjacency matrix and the effective resistance matrix.
-
-        Parameters:
-            node1 (str): The name of a node in the network.
-            node2 (str): The name of a node in the network.
-
-        Raises:
-            ValueError: If either node1 or node2 is not in the graph.
-        """
-        raise NotImplementedError("Problem 5 Incomplete")
+    n, n = np.shape(A)
+    D = np.zeros(n)
+    for i in range(n):
+        D[i] = np.sum(A[:,i])
+    
+    D = np.diag(D)
+    L = np.subtract(D, A)
+    Ld = drazin_inverse(L, tol=1e-4)
+    Ri,Rj = np.shape(Ld)
+    
+    for j in range(Rj):
+        Ld[j,j] = 0
+        
+    return Ld
+print(effective_resistance(A))
